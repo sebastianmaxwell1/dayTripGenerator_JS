c5c75aae051fa82b24003bbd6684bd1bbd86f8fa
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-05-12 20:23:28 -0400

    Created destinations array/console prints random places

--- daytripgen.js
+++ daytripgen.js
@@ -1,8 +1,18 @@
+"use strict";
+console.log("Hello")
 
-function generateTrip(){
+    // let destinationArray = ["Florida", "Ireland", "Japan", "Kansas", "Guam"]
+    // let restaurantArray = ["Seafood", "Local Pub", "Sushi", "Chipotle", "Hard Rock Cafe"];
+    // let transportationArray = ["Plane", "Boat", "Car", "Unicycle"];
+    // let entertainmentArray = ["Live Music", "Theater", "Sporting Events", "Alcohol"];
+
     let destinationArray = ["Florida", "Ireland", "Japan", "Kansas", "Guam"]
-    let restaurantArray = ["Seafood", "Local Pub", "Sushi", "Chipotle", "Hard Rock Cafe"];
-    let transportationArray = ["Plane", "Boat", "Car", "Unicycle"];
-    let entertainmentArray = ["Live Music", "Theater", "Sporting Events", "Alcohol"];
+    function destinationSelector() {
+        let destinations = Math.floor(Math.random() * destinationArray.length);
+        return destinationArray[destinations];
+    }
+    let result = destinationSelector();
+    console.log(result);
+
 
 
